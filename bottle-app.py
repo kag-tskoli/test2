48956481343feb20063d70053157e4c9e38b84cc
Karl Ágústsson <kag@tskoli.is>
2018-01-11 09:33:42 +0000

    Update bottle-app.py
    import os
    if statement for run()

--- bottle-app.py
+++ bottle-app.py
@@ -1,7 +1,11 @@
 from bottle import route, run
+import os
 
 @route('/')
 def index():
   return "Hello bottle"
   
-run()
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
