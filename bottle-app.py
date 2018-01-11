03afccf5b712c629730bf412155f3218207a719f
Karl Ágústsson <kag@tskoli.is>
2018-01-11 09:46:27 +0000

    Update bottle-app.py
    fixed indent in if statement

--- bottle-app.py
+++ bottle-app.py
@@ -6,6 +6,6 @@ def index():
   return "Hello bottle"
   
 if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+  run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
-    run(host='localhost', port=8080, debug=True)
+  run(host='localhost', port=8080, debug=True)
