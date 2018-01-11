69bac1bf131d71383fb1765d18d6d7759633e92e
Karl Ágústsson <kag@tskoli.is>
2018-01-11 09:48:50 +0000

    Update bottle-app.py
    removed if statement for run()

--- bottle-app.py
+++ bottle-app.py
@@ -4,8 +4,6 @@ import os
 @route('/')
 def index():
   return "Hello bottle"
-  
-if os.environ.get('APP_LOCATION') == 'heroku':
-  run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-  run(host='localhost', port=8080, debug=True)
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
