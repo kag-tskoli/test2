78a6d213b5f0cc8399c1c678141dbb60c73ed027
Karl Ágústsson <kag@tskoli.is>
2018-01-17 07:59:56 +0000

    Update bottle-app.py
    Added links in index.
    Added routes about, staff and contact

--- bottle-app.py
+++ bottle-app.py
@@ -3,11 +3,19 @@ import os
 
 @route('/')
 def index():
-  return "<h1>Hello bottle</h1>"
+  return "<a href="/about">About</a> <a href="/staff">Staff</a> <a href="/contact">Contact</a>
 
-@route('/hello')
+@route('/about')
 def hello():
-  return "This is route hello"
+  return "<h2>About us</h2>"
+
+@route('/staff')
+def staff():
+  return "<h2>Our staff</h2>"
+
+@route('/contact')
+def contact():
+  return "<h2>Contact us</h2>"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
